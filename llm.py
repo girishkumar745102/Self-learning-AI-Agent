a206ad0faef51cd4fa81792520c9d05cb01e8e33
girishkuamr745102 <girishkumar745102@gmail.com>
2026-08-27 11:47:29 +0530

    second commit

---
+++ llm.py
@@ -0,0 +1,40 @@
+"""
+llm.py
+-------
+This module communicates with the Groq API and provides memory
+to the LLM as context so that it can give personalized/relevant responses.
+"""
+
+import os
+from groq import Groq
+
+client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+
+def generate_response(user_message: str, relevant_memories: list[str]) -> str:
+    if relevant_memories:
+        memory_context = "\n".join(f"- {m}" for m in relevant_memories)
+    else:
+        memory_context = "No previous memory found"
+
+    system_prompt = f"""you are a helpfull AI assistant that learns abourt
+    the user and remember things.
+    Below are some known facts about the user (retrieved from memory).
+    Use these to give a persionalized response,
+    if relevant
+
+    known facts about user:
+    {memory_context}"""
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {"role": "system","content": system_prompt},
+            {"role": "user","content":user_message},
+        ],
+
+    )
+    return response.choices[0].message.content
+
+
+
+
